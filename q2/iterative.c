615c4c8fb70b0e8c33786564add8fe3520adb3fc
Faker <giranntu@gmail.com>
2015-10-14 16:39:57 +0800

    done ver1.
    1.use stdin for input
    2.commit iterative and recursive these two type of code

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,26 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c)
+{
+    int i=0;
+    while(str[i]!='\0') {
+        if(str[i]>c)
+            return str[i];
+        i++;
+    }
+    return str[0];
+}
 
 int main()
 {
+    char str[100];
+    char c;
+    printf("Enter the string : ");
+    gets(str);
+    printf("Enter the search character : ");
+    scanf("%c",&c);
+    fflush(stdin);
+    printf("%c\n",smallest_character(str, c));
+
     return 0;
 }
